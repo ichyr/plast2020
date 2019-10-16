cdff9ca4516a21b7d6bfe2fa325d17ba099ffbbe
Andrii Matsiievskyi <amatsiievskyi@lohika.com>
2019-10-16 21:10:50 +0300

    Added webpack to build js and scss

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # plast2020
+
+`webpack -p` to build  project
+
+all scss and js are build to bundle.js in dist folder
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: "/public"
+    },
+    // plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: { presets: ["es2015"] }
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "sass-loader" // compiles Sass to CSS
+                    }
+                ]
+            }
+        ]
+    }
+};

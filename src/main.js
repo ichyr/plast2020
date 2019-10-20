1353e5e6dae32084a67a276bfb81925915b4fc40
Andrii Matsiievskyi <amatsiievskyi@lohika.com>
2019-10-20 11:14:47 +0300

    Latest build

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import './support-banner.scss';
 import  './attraction.scss';
 import './donations_counter.scss';
 import './project-list.scss';
+import './about.scss';
+import './supporters.scss';
+import './contacts.scss';
+import './account_payment.scss';
